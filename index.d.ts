cc3efd08c735a680f170996c9a55c9470fb634d0
Martin Treurnicht <martin@step.com>
2021-01-27 12:10:49 -0800

    Fix typescript export

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,13 @@
 declare module 'react-native-aes-crypto' {
-    function pbkdf2(password: string, salt: string, cost: number, length: number): Promise<string>;
-    function encrypt(text: string, key: string, iv: string): Promise<string>;
-    function decrypt(ciphertext: string, key: string, iv: string): Promise<string>;
-    function hmac256(ciphertext: string, key: string): Promise<string>;
-    function randomKey(length: number): Promise<string>;
-    function sha1(text: string): Promise<string>;
-    function sha256(text: string): Promise<string>;
-    function sha512(text: string): Promise<string>;
+    namespace Aes {
+        function pbkdf2(password: string, salt: string, cost: number, length: number): Promise<string>;
+        function encrypt(text: string, key: string, iv: string): Promise<string>;
+        function decrypt(ciphertext: string, key: string, iv: string): Promise<string>;
+        function hmac256(ciphertext: string, key: string): Promise<string>;
+        function randomKey(length: number): Promise<string>;
+        function sha1(text: string): Promise<string>;
+        function sha256(text: string): Promise<string>;
+        function sha512(text: string): Promise<string>;
+    }
+    export default Aes
 }
